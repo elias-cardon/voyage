565a4e410bb7938332cb2d11310b42b16b072c72
antoineLGA <antoine.le-guillou@laplateforme.io>
2020-04-17 11:08:58 +0200

    first design landing_page and cleaning files

--- src/js/navScrollToStick.js
+++ src/js/navScrollToStick.js
@@ -5,12 +5,24 @@ function navScrollToStick() {
 
         if (scrollValue <= navFromTop) {
             $('nav').css({
-                'position': 'static'
-            })
+                'position': 'absolute',
+                'bottom': '0',
+                'top': 'unset'
+            });
+            $('div.dropup').removeClass('d-none');
+            $('div.dropdown').addClass('d-none');
+
+
         } else {
             $('nav').css({
-                'position': 'fixed'
-            })
+                'position': 'fixed',
+                'top': '0',
+                'bottom': 'unset'
+            });
+
+            $('div.dropup').addClass('d-none');
+            $('div.dropdown').removeClass('d-none');
+
         }
 
     });
